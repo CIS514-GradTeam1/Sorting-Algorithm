83494e93761231bcda359d86374130b164a3a4d5
mizanhemani <mizanhemani@gmail.com>
2021-02-08 16:20:13 -0600

    added quick sort

---
+++ QuickSort.cs
@@ -0,0 +1,38 @@
+import System;
+
+namespace SortingAlgo
+{
+    class QuickSort
+    {
+        public static List<T> QuickSort<T>(List<T> elements) where T : IComparable
+        {
+            var rand = new Random();
+
+            if(elements.Count < 2) return elements;
+
+            var pivot = rand.Next(0, elements.Count-1);
+            var val = elements[pivot];
+
+            var lesser = new List<T>();
+            var greater = new List<T>();
+            for (var i = 0; i < elements.Count; ++i)
+            {
+                if (i != pivot)
+                {
+                    if (elements[i].CompareTo(val) < 0)
+                    {
+                        lesser.Add(elements[i]);
+                    }
+                    else
+                    {
+                        greater.Add(elements[i]);
+                    }
+                }
+            }
+
+            var merged = QuickSort(lesser);
+            merged.Add(val);
+            merged.AddRange(QuickSort(greater));
+            return merged;
+        }
+}
